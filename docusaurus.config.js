cec61f47f2a196d405aa8cc0c1ae8592eaa6e462
Bart <bartvtende@gmail.com>
2022-12-17 21:47:06 +0100

    bart/Docusaurus setup

---
+++ docusaurus.config.js
@@ -0,0 +1,103 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'IJsbaan Lageland',
+  tagline: 'IJsbaan Lageland Hamweg',
+  url: 'https://ijslaanlageland.nl',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  // favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'bartvtende', // Usually your GitHub org/user name.
+  projectName: 'ijsbaanlageland', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'nl',
+    locales: ['nl'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+      },
+      navbar: {
+        title: 'IJsbaan Lageland',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
+        items: [
+          {to: '/contact', label: 'Contact', position: 'left'},
+        ],
+      },
+      // footer: {
+      //   style: 'dark',
+      //   links: [
+      //     {
+      //       title: 'Community',
+      //       items: [
+      //         {
+      //           html: `
+      //             <
+      //           `
+      //         },
+      //         {
+      //           label: 'Discord',
+      //           href: 'https://discordapp.com/invite/docusaurus',
+      //         },
+      //         {
+      //           label: 'Twitter',
+      //           href: 'https://twitter.com/docusaurus',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'More',
+      //       items: [
+      //         {
+      //           label: 'Blog',
+      //           to: '/blog',
+      //         },
+      //         {
+      //           label: 'GitHub',
+      //           href: 'https://github.com/facebook/docusaurus',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      //   copyright: `Copyright © ${new Date().getFullYear()} IJsclub Lageland Hamweg`,
+      // },
+    }),
+
+  scripts: [
+    {
+      src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initialize&v=weekly',
+      defer: true,
+    }
+  ]
+};
+
+module.exports = config;
---
+++ src/components/IjsbaanOpen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function IjsbaanOpen() {
+    return (
+        <div className={styles.openBanner}>
+            <h2>De IJsbaan is zondag 18 december geopend vanaf 10:00 uur!</h2>
+        </div>
+    );
+}
